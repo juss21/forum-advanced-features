14dd0c582a19667c9fa4c6d9e809015f2fa16c0c
kasepuu <joel.meeks@outlook.com>
2023-01-25 14:35:06 +0200

    basic sql

--- main.go
+++ main.go
@@ -1,31 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"text/template"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	port := "8080"                                                                    // webserver port
-	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web")))) // handling web folder
-	http.HandleFunc("/", serverHandle)                                                // server handle
-	fmt.Printf("Starting server at port " + port + "\n")
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal(err)
+	//port := "8080" // webserver port
+	database, _ := sql.Open("sqlite3", "./members.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?,?)")
+	statement.Exec("Red", "Ferrari") //exec first name, last name
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+
+	var id int
+	var name string
+	var password string
+	for rows.Next() {
+		rows.Scan(&id, &name, &password)
+		fmt.Println(id, " ", name, " ", password)
 	}
+	// fmt.Println("statement: ", statement)
+	// fmt.Println("database: ", database)
+
+	// http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web")))) // handling web folder
+	// http.HandleFunc("/", serverHandle)                                                // server handle
+	// fmt.Printf("Starting server at port " + port + "\n")
+	// err := http.ListenAndServe(":"+port, nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 }
 
 func serverHandle(w http.ResponseWriter, r *http.Request) {
 	homepage, err := template.ParseFiles("web/index.html") // home template
 	errorCheck(err, true)
+	errorpage, err2 := template.ParseFiles("web/error.html") //error template
+	errorCheck(err2, true)
 
 	if r.Method == "GET" {
 		if r.URL.Path == "/" {
 			homepage.Execute(w, "") // at homepage print homepage
+		} else {
+			errorpage.Execute(w, "")
 		}
 
 	}
