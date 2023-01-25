9e2bff92f78c99f6124209365f043ae9bb277f6d
kasepuu <joel.meeks@outlook.com>
2023-01-25 12:56:43 +0200

    add

--- main.go
+++ main.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
 
 func main() {
-	fmt.Println("starting server: ")
+	port := "8080"                                                                    // webserver port
+	http.Handle("/web/", http.StripPrefix("/web/", http.FileServer(http.Dir("web")))) // handling web folder
+	http.HandleFunc("/", serverHandle)                                                // server handle
+	fmt.Printf("Starting server at port " + port + "\n")
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
-	fmt.Println("exiting...")
+func serverHandle(w http.ResponseWriter, r *http.Request) {
+	homepage, err := template.ParseFiles("web/index.html") // home template
+	errorCheck(err, true)
+
+	if r.Method == "GET" {
+		if r.URL.Path == "/" {
+			homepage.Execute(w, "") // at homepage print homepage
+		}
+
+	}
+}
+
+func errorCheck(err error, exit bool) {
+	if err != nil {
+		fmt.Println(err)
+		if exit {
+			os.Exit(0)
+		}
+	}
 }
