3ebc1be2b775ec4829b6a1cf5055abff1d4a5d71
kasepuu <joel.meeks@outlook.com>
2023-02-02 05:16:09 +0200

    add

---
+++ sqlfunctions.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func AllPosts() []Forumdata {
+	var data []Forumdata
+	rows, err := DataBase.Query("SELECT posts.id, users.username, posts.title, posts.content, date FROM posts LEFT JOIN users on posts.userId = users.id")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var post Forumdata
+		rows.Scan(
+			&post.Id,
+			&post.Author,
+			&post.Title,
+			&post.Content,
+			&post.Date_posted,
+		)
+		data = append(data, post)
+	}
+
+	return data
+}
+
+func SavePost(title string, author int, content string) bool {
+	if len(title) < 5 {
+		Web.ErrorMsg = "Too few characters in post title!"
+		return false
+	}
+	if len(content) < 10 {
+		Web.ErrorMsg = "Too few characters in post content! SPAM?"
+		return false
+	}
+	statement, _ := DataBase.Prepare("INSERT INTO posts (userId, title, content) VALUES (?,?,?)")
+
+	statement.Exec(author, title, content)
+
+	return true
+}
+
+func GetPostById(postId int) Forumdata {
+	var post Forumdata
+	statement, _ := DataBase.Prepare(`SELECT 
+	posts.id, posts.userId, posts.title, posts.content,  
+	COUNT(CASE WHEN postlikes.name = 'like' THEN 1 END) AS likes, 
+	COUNT(CASE WHEN postlikes.name = 'dislike' THEN 1 END) AS dislikes  
+  FROM 
+	posts 
+	LEFT JOIN postlikes ON posts.id = postlikes.postId	
+	WHERE posts.id = ?
+	GROUP by posts.id
+  `)
+	err := statement.QueryRow(postId).Scan(
+		&post.Id,
+		&post.Author,
+		&post.Title,
+		&post.Content,
+		&post.Likes,
+		&post.Dislikes,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return post
+}
+
+func SaveComment(content string, userId, postId int) bool {
+	if len(content) < 10 {
+		Web.ErrorMsg = "Too few arguments in commentbox. Spam?"
+		return false
+	}
+
+	statement, _ := DataBase.Prepare("INSERT INTO comments (userId, content, postId) VALUES (?,?,?)")
+	statement.Exec(userId, content, postId)
+	return true
+}
+
+func Login(username, password string) (Memberlist, error) {
+	var user Memberlist
+	statement, _ := DataBase.Prepare("SELECT id, username, email FROM users WHERE username=? and password=?")
+	err := statement.QueryRow(username, password).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+	)
+	if err == sql.ErrNoRows {
+		return Memberlist{}, err
+	}
+
+	return user, err
+}
+
+func Register(username, password, email string) {
+	statement, _ := DataBase.Prepare("INSERT INTO users (username, password, email) values (?,?,?)")
+	statement.Exec(username, password, email)
+}
+
+func GetUsers() []Memberlist {
+	var data []Memberlist
+	rows, _ := DataBase.Query("SELECT id, username, email from users")
+
+	for rows.Next() {
+		var member Memberlist
+		rows.Scan(
+			&member.ID,
+			&member.Username,
+			&member.Email,
+		)
+		data = append(data, member)
+	}
+
+	return data
+}
+
+func GetCommentsByPostId(id int) []Commentdata {
+	var comments []Commentdata
+	statement, _ := DataBase.Prepare(`
+	SELECT 
+	comments.id, comments.userId, comments.content, 
+  COUNT(CASE WHEN commentLikes.name = 'like' THEN 1 END) AS likes, 
+  COUNT(CASE WHEN commentLikes.name = 'dislike' THEN 1 END) AS dislikes  
+FROM 
+  comments 
+  LEFT JOIN commentLikes ON comments.id = commentLikes.commentId  
+  WHERE comments.postId= ?
+  GROUP by comments.id
+	`)
+	rows, err := statement.Query(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var comment Commentdata
+		rows.Scan(
+			&comment.Id,
+			&comment.UserId,
+			&comment.Content,
+			&comment.Likes,
+			&comment.Dislikes,
+		)
+		comments = append(comments, comment)
+	}
+
+	return comments
+}
+
+func SavePostLike(like string, userId, postId int) {
+	var dbLike string
+	statement, _ := DataBase.Prepare("SELECT name FROM postlikes WHERE  userId = ? and postId = ?")
+	statement.QueryRow(userId, postId).Scan(&dbLike)
+	if like == dbLike {
+		toggleLike, _ := DataBase.Prepare("DELETE FROM postlikes WHERE  userId = ? and postId = ?")
+		toggleLike.Exec(userId, postId)
+	} else {
+		toggleLike, _ := DataBase.Prepare("DELETE FROM postlikes WHERE  userId = ? and postId = ?")
+		toggleLike.Exec(userId, postId)
+		saving, _ := DataBase.Prepare("INSERT INTO postlikes (name, userId, postId) VALUES (?,?,?)")
+		_, err := saving.Exec(like, userId, postId)
+		if err == nil {
+			return
+		}
+	}
+}
+
+func SaveCommentLike(like string, userId, commentId int) {
+	var dbLike string
+	statement, _ := DataBase.Prepare("SELECT name FROM commentLikes WHERE  userId = ? and commentId = ?")
+	statement.QueryRow(userId, commentId).Scan(&dbLike)
+	if like == dbLike {
+		toggleLike, _ := DataBase.Prepare("DELETE FROM commentLikes WHERE  userId = ? and commentId = ?")
+		toggleLike.Exec(userId, commentId)
+	} else {
+		toggleLike, _ := DataBase.Prepare("DELETE FROM commentLikes WHERE  userId = ? and commentId = ?")
+		toggleLike.Exec(userId, commentId)
+		saving, _ := DataBase.Prepare("INSERT INTO commentLikes (name, userId, commentId) VALUES (?,?,?)")
+		_, err := saving.Exec(like, userId, commentId)
+		if err == nil {
+			return
+		}
+	}
+}
+
+func SaveSession(key string, userId int) {
+	statement, _ := DataBase.Prepare("INSERT INTO session (key, userId) VALUES (?,?)")
+	_, err := statement.Exec(key, userId)
+
+	if err != nil {
+		fmt.Println("one per user")
+	}
+}
--- structs.go
+++ structs.go
@@ -1,98 +1,48 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	"os"
-)
+import "database/sql"
 
-type memberlist struct {
-	ID          int
-	Username    string
-	Password    string
-	DateCreated string
-	Email       string
+type Memberlist struct {
+	ID       int
+	Username string
+	Password string
+	Email    string
 }
-type forumfamily struct {
-	ID             int
-	Originalposter string
-	Post_title     string
-	Post_content   string
-	Commentor_data []commentpandemic
-	Date_posted    string
-	Post_likes     int
-	Post_disLikes  int
-	Loggedin       bool
-	Currentuser    string
+type Forumdata struct {
+	Id          int
+	Author      string
+	Title       string
+	Content     string
+	Date_posted string
+	Comments    []Commentdata
+	Likes       int
+	Dislikes    int
+	Loggedin    bool
 }
-type commentpandemic struct {
-	ID               int
-	Date             string
-	Commentor        string
-	Forum_comment    string
-	Post_header      string
-	Comment_likes    int
-	Comment_disLikes int
+type Commentdata struct {
+	Id       int
+	Content  string
+	UserId   int
+	PostId   int
+	Likes    int
+	Dislikes int
 }
 
-type commentlikes struct {
-	CommentID int
-	UserID    int
-	Status    int
-}
-type commentdislikes struct {
-	CommentID int
-	UserID    int
-	Status    int
+type PostLike struct {
+	Id     int
+	Name   string
+	UserId int
+	PostId int
 }
 
-type forumlikes struct {
-	TopicID int
-	UserID  int
-	Status  int
-}
-type forumdislikes struct {
-	TopicID int
-	UserID  int
-	Status  int
-}
-type webstuff struct {
-	Loggedin        bool
-	Currentuser     string
-	Currentpage     string
-	Sqlbase         *sql.DB
-	TopicDisLikes   []forumdislikes
-	TopicLikes      []forumlikes
-	CommentLikes    []commentlikes
-	CommentDisLikes []commentdislikes
-	Userlist        []memberlist
-	Forum_data      []forumfamily
-	allposts        int
-	allcomments     int
-	tempint         int
-	tempcomments    []commentpandemic
-	ErrorMsg        string
+type Forumstuff struct {
+	Loggedin    bool
+	LoggedUser  Memberlist
+	CurrentPost Forumdata
+	Forum_data  []Forumdata
+	User_data   []Memberlist
+	ErrorMsg    string
 }
 
-var (
-	Web   webstuff
-	debug bool = true
-)
-
-// function for errorchecking
-func errorCheck(err error, exit bool) {
-	if err != nil {
-		fmt.Println(err)
-		if exit {
-			os.Exit(0)
-		}
-	}
-}
-
-// function for log printing, can be turned off by setting debug boolean to false
-func printLog(a ...any) (n int, err error) {
-	if debug {
-		return fmt.Fprintln(os.Stdout, a...)
-	}
-	return 0, nil
-}
+var DataBase *sql.DB
+var Web Forumstuff
