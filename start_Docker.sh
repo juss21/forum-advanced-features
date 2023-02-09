77ea1236fd18d3e09d4fdd56b8662a34cd5a328e
andreiRedi <andrei.redi@gmail.com>
2023-02-09 10:23:40 +0200

    Server runnable in docker, deletes image and container with script

---
+++ start_Docker.sh
@@ -0,0 +1,36 @@
+ #!/bin/bash
+
+echo "Building docker image and container"
+echo 
+echo "🎉🎉🎉To stop server, press CTRL+C🎉🎉🎉"
+echo "🎉🎉🎉   Press Enter to Continue  🎉🎉🎉"
+
+echo ""
+echo ""
+echo ""
+
+read "part"
+
+
+
+
+docker image build -f Dockerfile -t dockerize-image .
+docker container run -p 8080:8080  --name dockerize-container dockerize-image
+
+echo ""
+echo ""
+echo ""
+
+echo "Removing Docker image and container from your system"
+echo ""
+echo ""
+docker rm -f dockerize-container
+docker image rm dockerize-image
+
+
+echo "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉"
+echo "DONE"
+
+
+
+
